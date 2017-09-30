bd01293e755d19a398804d2fc9e4b13a295e7567
TM Lee <tm89lee@gmail.com>
2017-10-01 01:40:10 +0800

    Use VCR to record API response with a way to expire

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require "bundler/setup"
 require "cryptoexchange"
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -7,5 +13,20 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  # Add VCR to all tests
+  # Usages
+  # > VCR=all rspec spec # This will force all casettes to re-record
+  # > rspec spec # This will save a casette if it does not exist, and reuse in the next
+  config.around(:each) do |example|
+    record_mode = ENV["VCR"] ? ENV["VCR"].to_sym : :once
+    options = example.metadata[:vcr] || { :record => record_mode }
+    if options[:record] == :skip
+      VCR.turned_off(&example)
+    else
+      name = example.metadata[:full_description].split(/\s+/, 2).join('/').gsub(' ','_').gsub(/\./,'/').gsub(/[^\w\/]+/, '_').gsub(/\/$/, '')
+      v = VCR.use_cassette(name, options, &example)
+    end
   end
 end
